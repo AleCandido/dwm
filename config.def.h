4661112eab9808a9dd835fb0376964c1ce71d392
AleCandido <candido.ale@gmail.com>
2020-05-01 19:11:04 +0200

    Add tint2 to rules

--- config.def.h
+++ config.def.h
@@ -22,13 +22,18 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    /* xprop(1):
+     *  use win_info to find
+     *
+     *  WM_CLASS(STRING) = instance, class
+     *  WM_NAME(STRING) = title
+     */
+    /* class      instance    title       tags mask     isfloating   monitor */
+    //{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+    // zoom share screen - external frame has only title
+    { NULL,       NULL,       "cpt_frame_window",       0,            1,           0 },
+    // tint2 bar - make it floating on top
+    { "Tint2",    NULL,       NULL,       0,            1,           0 },
 };
 
 /* layout(s) */
